9bc99e35c8ae90ea54be1d835f28b58f0b4d0485
dgpcamara <lfcamara.ti@gmail.com>
2017-10-31 10:25:46 -0200

    Teste

--- Angular2-AdminLTE-master/src/app/app.module.ts
+++ Angular2-AdminLTE-master/src/app/app.module.ts
@@ -19,6 +19,8 @@ import { AdminControlSidebarComponent } from './admin/admin-control-sidebar/admi
 import { AdminDashboard1Component } from './admin/admin-dashboard1/admin-dashboard1.component';
 import { TesteComponent } from './teste/teste.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
